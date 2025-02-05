bfd939be01b4734fb50304bf3989c9ae651b9c51
Miki <mickeytuts2401@gmail.com>
2025-02-05 16:27:21 +0100

    Add Radnik Add and List pages with functionalities

--- SZTCR_01/Controllers/AdministratorController.cs
+++ SZTCR_01/Controllers/AdministratorController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SZTCR_01.Data;
+using SZTCR_01.Models.Domain;
+using SZTCR_01.Models.ViewModels;
 
 namespace SZTCR_01.Controllers
 {
     public class AdministratorController : Controller
     {
-        public IActionResult Index()
+        private readonly SztcrDbContext sztcrDbContext;
+
+        public AdministratorController(SztcrDbContext sztcrDbContext)
+        {
+            this.sztcrDbContext = sztcrDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeRequest addEmployeeRequest)
+        {
+
+            var radnik = new Radnik
+            {
+                Ime = addEmployeeRequest.Ime,
+                Prezime = addEmployeeRequest.Prezime,
+                Pozicija = addEmployeeRequest.Pozicija
+            };
+
+            sztcrDbContext.Radnici.Add(radnik);
+            sztcrDbContext.SaveChanges();
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var radnici = sztcrDbContext.Radnici.ToList();
+
+            return View(radnici);
         }
     }
 }
---
+++ SZTCR_01/Models/ViewModels/AddEmployeeRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SZTCR_01.Models.ViewModels
+{
+    public class AddEmployeeRequest
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Pozicija { get; set; }
+    }
+}
