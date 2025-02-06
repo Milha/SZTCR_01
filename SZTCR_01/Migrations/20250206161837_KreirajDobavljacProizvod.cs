977b14ec1b6fd0cb72a9c76c55af9a0a1ce79cf0
Miki <mickeytuts2401@gmail.com>
2025-02-06 18:31:12 +0100

    Add AddProduct1

---
+++ SZTCR_01/Migrations/20250206161837_KreirajDobavljacProizvod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SZTCR_01.Migrations
+{
+    /// <inheritdoc />
+    public partial class KreirajDobavljacProizvod : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Proizvodi",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdDobavljaca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NazivProizvoda = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Kolicina = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Proizvodi", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Proizvodi");
+        }
+    }
+}
---
+++ SZTCR_01/Models/ViewModels/AddProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace SZTCR_01.Models.ViewModels
+{
+    public class AddProductRequest
+    {
+        public Guid IdDobavljaca { get; set; }
+        public string NazivProizvoda { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
